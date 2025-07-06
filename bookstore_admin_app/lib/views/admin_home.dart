3cd93bd5d285334e32d9398055fd065930996092
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-06 15:10:09 +0530

    admin dashbord create with text & btns for actions

---
+++ bookstore_admin_app/lib/views/admin_home.dart
@@ -0,0 +1,59 @@
+import 'package:bookstore_admin_app/containers/dashboard_text.dart';
+import 'package:bookstore_admin_app/containers/home_button.dart';
+import 'package:flutter/material.dart';
+
+class AdminHome extends StatefulWidget {
+  const AdminHome({super.key});
+
+  @override
+  State<AdminHome> createState() => _AdminHomeState();
+}
+
+class _AdminHomeState extends State<AdminHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Admin Dashboard")),
+      body: Column(
+        children: [
+          Container(
+            height: 235,
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DashboardText(keyword: "Total Books", value: "100"),
+                DashboardText(keyword: "Total Books", value: "100"),
+                DashboardText(keyword: "Total Books", value: "100"),
+                DashboardText(keyword: "Total Books", value: "100"),
+                DashboardText(keyword: "Total Books", value: "100"),
+              ],
+            ),
+          ),
+          //Buttons for admin
+          Row(
+            children: [
+              HomeButton(onTap: () {}, name: "All Orders"),
+              HomeButton(onTap: () {}, name: "All Books"),
+            ],
+          ),
+          Row(
+            children: [
+              HomeButton(onTap: () {}, name: "Promos"),
+              HomeButton(onTap: () {}, name: "Banners"),
+            ],
+          ),
+          Row(
+            children: [HomeButton(onTap: () {}, name: "Categories")],
+          ),
+        ],
+      ),
+    );
+  }
+}
