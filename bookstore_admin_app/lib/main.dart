a19bcb1ffcfbdf3b9729869c51a83a955c0f8f6c
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-06 23:14:49 +0530

    check user session and direct to homepage or login

--- bookstore_admin_app/lib/main.dart
+++ bookstore_admin_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookstore_admin_app/controlllers/auth_service.dart';
 import 'package:bookstore_admin_app/views/admin_home.dart';
 import 'package:bookstore_admin_app/views/login.dart';
 import 'package:bookstore_admin_app/views/signup.dart';
@@ -22,11 +23,38 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       routes: {
-        "/": (context) => LoginPage(),
+        "/": (context) => CheckUser(),
         "/login": (context) => LoginPage(),
         "/signup": (context) => SignupPage(),
         "/home": (context) => AdminHome(),
       },
     );
+  }
+}
+
+//check user state - loggedin/or not
+class CheckUser extends StatefulWidget {
+  const CheckUser({super.key});
+
+  @override
+  State<CheckUser> createState() => _CheckUserState();
+}
+
+class _CheckUserState extends State<CheckUser> {
+  @override
+  void initState() {
+    AuthService().isLoggedIn().then((value) {
+      if (value) {
+        Navigator.pushReplacementNamed(context, "/home");
+      } else {
+        Navigator.pushReplacementNamed(context, "/login");
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: CircularProgressIndicator()));
   }
 }
