1e449a7c4f73af292ab984fea9dd871b23e0f3a5
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-06 15:57:10 +0530

    link firebase with app

--- bookstore_admin_app/lib/main.dart
+++ bookstore_admin_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:bookstore_admin_app/views/admin_home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
