99c7ec5f020a2fe1835abc0c4a1ea2f6eb0e281c
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-07 14:39:38 +0530

    cloud firestore setup, CRUD functions for book categories collections

---
+++ bookstore_admin_app/lib/controlllers/db_service.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DbService {
+  // CATEGORIES
+  // read categories from database
+  Stream<QuerySnapshot> readCategories() {
+    return FirebaseFirestore.instance
+        .collection("book_categories")
+        .snapshots();
+  }
+
+  // create new category
+  Future createCategories({required Map<String, dynamic> data}) async {
+    await FirebaseFirestore.instance.collection("book_categories").add(data);
+  }
+
+  // update category
+  Future updateCategories(
+      {required String docId, required Map<String, dynamic> data}) async {
+    await FirebaseFirestore.instance
+        .collection("book_categories")
+        .doc(docId)
+        .update(data);
+  }
+
+  // delete category
+  Future deleteCategories({required String docId}) async {
+    await FirebaseFirestore.instance
+        .collection("book_categories")
+        .doc(docId)
+        .delete();
+  }
+}
